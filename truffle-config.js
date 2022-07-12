5f9801f861369b00732147aae4ef19a21580b65f
邵謙(Shau Chien) <cvcvbnbn13@gmail.com>
2022-07-12 09:57:03 +0800

    [feat]first-commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,16 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const keys = require('./keys.json');
 
 module.exports = {
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache, geth, or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*',
     },
     rinkeby: {
-      provider: () => new HDWalletProvider('PRIVATE_KEY', 'INFURA_RINKEBY_URL'),
+      provider: () =>
+        new HDWalletProvider(keys.PRIVATE_KEY, keys.INFURA_RINKEBY_URL),
       network_id: 4, // rinkeby's id
       gas: 5500000, // rinkeby has a lower block limit than mainnet
     },
@@ -31,7 +24,7 @@ module.exports = {
   },
 
   api_keys: {
-    etherscan: '',
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
   plugins: ['truffle-plugin-verify'],
 };
